8543741bfd66abe36cb825d9db11dec70ed4d475
Valkke <janivalkeapaa_94@hotmail.com>
2017-09-29 11:12:52 +0300

    Ready

--- conditional-statements/conditional-statement/task-4/Program.cs
+++ conditional-statements/conditional-statement/task-4/Program.cs
@@ -9,19 +9,78 @@ namespace task_4
     class Program
     {
         static void Main(string[] args)
- {
+        { 
+            // Define instructions
             Console.WriteLine("Ohjelma lajittelee luvut nousevaan järjestykseen");
-            bool.isNumber;
+
+            Console.WriteLine("Syötä numero 1");
+            string numero1 = Console.ReadLine();
+            int numx = int.Parse(numero1);
+
+            Console.WriteLine("Syötä numero 2");
+            string numero2 = Console.ReadLine();
+            int numy = int.Parse(numero2);
+
+            Console.WriteLine("Syötä numero 3");
+            string numero3 = Console.ReadLine();
+            int numz = int.Parse(numero3);
 
             
+            
 
+            if (numx < numy)
+            {
+                if (numx < numz)
+                 {
+                    if (numy < numz)
+                    {
+                        Console.WriteLine($"{numx}, {numy}, {numz}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{numx}, {numz}, {numy}");
+                    }
+                 }
+                else 
+                {            
+                  Console.WriteLine($"{numz}, {numx}, {numy}");
+                }
+            }
 
+            else
+            {
+                if (numx < numz)
+                {
+                    Console.WriteLine($"{numy}, {numx}, {numz}");
+                }
+                else
+                {
+                    if (numy < numz)
+                    {
+                        Console.WriteLine($"{numy}, {numz}, {numx}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{numz}, {numy}, {numx}");
+                    }
+                }
+            }
 
-
-
-
-
-
-        }
-    }
+    
+             Console.ReadKey();
+     }
+   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
