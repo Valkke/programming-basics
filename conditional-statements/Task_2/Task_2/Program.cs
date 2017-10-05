bdc24a95bc57b19e9989a7053718e4d86bfe172e
Valkke <janivalkeapaa_94@hotmail.com>
2017-10-05 11:20:13 +0300

    Harkka kakkonen

---
+++ conditional-statements/Task_2/Task_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName = "Uusi";
+            string LastName = "Asiakas";
+            Console.WriteLine(" " + FirstName + " "+ LastName);
+            Console.WriteLine("Kirjoita Nimesi");
+            FirstName = Console.ReadLine();
+            LastName = Console.ReadLine();
+
+            int number;
+            Console.WriteLine("Kirjoita ikäsi");
+            number = int.Parse (Console.ReadLine());
+            if (number < 7)
+                Console.WriteLine("Lippusi on ilmainen");
+            else
+            if (number > 65)
+                    Console.WriteLine("Saat 50% alennuksen eli lippusi maksaa 8 euroa");
+            
+            else
+            if (number > 6 && number < 16)
+                Console.WriteLine("Saat 50% alennuksen eli lippusi maksaa 8 euroa");
+
+            string jasenyys = "Oletko MTK:n jasen"
+
+
+
+
+
+            Console.ReadLine();
+
+
+           
+
+
+
+        }
+    }
+}
