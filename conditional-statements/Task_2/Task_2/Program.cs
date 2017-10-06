910368beca0a81036d7163028bd10c7729395084
Valkke <janivalkeapaa_94@hotmail.com>
2017-10-06 15:29:40 +0300

    harkka valmis

--- conditional-statements/Task_2/Task_2/Program.cs
+++ conditional-statements/Task_2/Task_2/Program.cs
@@ -10,15 +10,16 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
-            string FirstName = "Uusi";
-            string LastName = "Asiakas";
-            Console.WriteLine(" " + FirstName + " "+ LastName);
-            Console.WriteLine("Kirjoita Nimesi");
-            FirstName = Console.ReadLine();
-            LastName = Console.ReadLine();
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Maatalousnayttelyn lipunhinta");
+            int price = 16;
+            float discount = 0.0f;
+
+
 
             int number;
-            Console.WriteLine("Kirjoita ikäsi");
+            Console.Write("Ikä: ");
+            Console.WriteLine("Kirjoita ikasi");
             number = int.Parse (Console.ReadLine());
             if (number < 7)
                 Console.WriteLine("Lippusi on ilmainen");
@@ -30,19 +31,47 @@ namespace Task_2
             if (number > 6 && number < 16)
                 Console.WriteLine("Saat 50% alennuksen eli lippusi maksaa 8 euroa");
 
-            string jasenyys = "Oletko MTK:n jasen"
+            if (number > 15 && number < 65)
+            {
 
+            Console.Write("Oletko varusmies? {Y/N}");
+            if (Console.ReadLine ().ToUpper () == "Y")
+                Console.Write($"Lippu maksaa {price * 0.50}euroa ");
+            else 
+            {
+                Console.Write("Oletko MTK:n jasen? {Y/N}");
+                if (Console.ReadLine().ToUpper() =="Y")
+                discount = 0.15f;
 
-
-
-
-            Console.ReadLine();
-
-
-           
-
-
-
+                Console.Write("Oletko opiskelija?{Y/N}");
+                if (Console.ReadLine().ToUpper() =="Y")
+                {
+                    discount = discount + 0.45f;
+                }
+                Console.WriteLine($"Lippusi hinta on {price - price * discount}euroa");
+            }
         }
-    }
+        Console.ReadKey();
+   }
 }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
